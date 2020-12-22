6c72818466c7a46cfde6ef9b6b55479f400cd874
xyligan2222 <xyligan2222@yandex.ru>
2020-12-22 17:32:16 +0300

    4. candicates.jsp - список кандидатов. [#368656]

--- src/main/java/model/Store.java
+++ src/main/java/model/Store.java
@@ -10,10 +10,15 @@ public class Store {
 
     private Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));
         posts.put(2, new Post(2, "Middle Java Job"));
         posts.put(3, new Post(3, "Senior Java Job"));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
@@ -22,5 +27,9 @@ public class Store {
 
     public Collection<Post> findAll() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
