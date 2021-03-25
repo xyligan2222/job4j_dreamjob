452b9cfd0bb93bbc46bee17982c67cd2f2e56ca0
xyligan2222 <xyligan2222@yandex.ru>
2021-03-25 12:57:44 +0300

    4.1. JSON формат. [#368641]

--- src/main/java/servlet/GreetingServlet.java
+++ src/main/java/servlet/GreetingServlet.java
@@ -1,5 +1,8 @@
 package servlet;
 
+import com.google.gson.JsonObject;
+import org.json.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,13 +14,18 @@ public class GreetingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
+        resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
         String name = req.getParameter("name");
+        resp.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+        resp.setHeader("Access-Control-Allow-Methods", "GET");
+        resp.setHeader("Access-Control-Max-Age", "1728000");
         resp.setHeader("Access-Control-Allow-Origin", "*");
+        JSONObject json = new JSONObject();
+        json.put("email", name);
         System.out.println(name);
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("Email address " + name);
+        writer.println(json.toString());
         writer.flush();
     }
 }
